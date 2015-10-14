cb3cc119785ef918a73f3efb3adc749c7516f16b
mando57 <themando57@gmail.com>
2015-10-14 10:13:54 +0200

    deltaligne

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: cdincher
- * Date: 14/10/2015
- * Time: 09:48
- */
+* User: cdincher
+* Date: 14/10/2015
+* Time: 09:48
+*/
 
 echo "test Cedric n";
 
-echo "salut";
+echo " del ta ligne batard";
+
+
 ?>
